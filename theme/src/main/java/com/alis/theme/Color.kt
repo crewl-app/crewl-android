3e294927cc2ab38a72942baa384d79c4e93b1ac6
Kaan Fırat <frtpkaan@gmail.com>
2023-01-30 02:10:45 +0300

    fix: fix test dependencies
    feat: add theme settings

---
+++ theme/src/main/java/com/alis/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.alis.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SoftPeach = Color(0xFFF4ECE8)
+val BrightGold = Color(0xFFFECB18)
+val CarbonGray = Color(0xFF606060)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Transparent = Color(0x00000000)
+
+val BackgroundLight = Color(0xFFF4ECE8)
+val BackgroundDark = Color(0xFF24292E)
+
+val DividerLight = Color(0xFFE0E0E0)
+val DividerDark = Color(0xFF6E6E6E)
+
+val Gray25 = Color(0xFFF8F8F8)
+val Gray50 = Color(0xFFF1F1F1)
+val Gray75 = Color(0xFFECECEC)
+val Gray100 = Color(0xFFE1E1E1)
+val Gray200 = Color(0xFFEEEEEE)
+val Gray300 = Color(0xFFACACAC)
+val Gray400 = Color(0xFF919191)
+val Gray500 = Color(0xFF6E6E6E)
+val Gray600 = Color(0xFF535353)
+val Gray700 = Color(0xFF616161)
+val Gray800 = Color(0xFF292929)
+val Gray900 = Color(0xFF212121)
+val Gray950 = Color(0xFF141414)
---
+++ theme/src/main/java/com/alis/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.alis.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val SpaceGrotesk = FontFamily(
+    Font(R.font.space_grotesk_light, weight = FontWeight.Light),
+    Font(R.font.space_grotesk, weight = FontWeight.Normal),
+    Font(R.font.space_grotesk_medium, weight = FontWeight.Medium),
+    Font(R.font.space_grotesk_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.space_grotesk_bold, weight = FontWeight.Bold)
+)
